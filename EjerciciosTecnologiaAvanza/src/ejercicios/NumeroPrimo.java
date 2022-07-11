078358e3392d8395a0733a3cfe6a2ba8e3f7d359
Marco Antonio Dominguez <34685744+marcklinux@users.noreply.github.com>
2022-07-11 09:22:18 -0500

    ejercicio-numeroPrimo

--- EjerciciosTecnologiaAvanza/src/ejercicios/NumeroPrimo.java
+++ EjerciciosTecnologiaAvanza/src/ejercicios/NumeroPrimo.java
@@ -1,13 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package ejercicios;
 
-/**
- *
- * @author marck
- */
+import java.util.Scanner;
+
+
 public class NumeroPrimo {
-    
+    public static void main(String[] args) {
+         Scanner teclado = new Scanner(System.in);
+         
+         int numero;
+         int contador=0;
+         
+         System.out.println("Digite un numero: ");
+         numero = teclado.nextInt();
+         
+         for(int i= 1; i <= numero;i++){
+             if(numero % i == 0){
+                 contador++;
+             }
+         }
+         
+         if(contador == 2){
+             System.out.println("El numero es primo");
+         }else{
+             System.out.println("El numero no es primo");
+         }
+    }
 }
